ed5c8c62d04c33458d88ce3739564e543ce8c2bc
Daniele Della Cioppa <daniele.dellacioppa@gmail.com>
2023-06-10 17:48:01 +0100

    button

--- app/src/main/java/com/poetic/FluentCantonese/MainActivity.java
+++ app/src/main/java/com/poetic/FluentCantonese/MainActivity.java
@@ -2,13 +2,35 @@ package com.poetic.FluentCantonese;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.poetic.FluentCantonese.activities.FlashcardActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button button = null;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setButtons();
+    }
+
+    void setButtons()
+    {
+        button = (Button) findViewById(R.id.buttonFlashcard);
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(MainActivity.this, FlashcardActivity.class);
+                MainActivity.this.startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/poetic/FluentCantonese/activities/FlashcardActivity.java
@@ -0,0 +1,16 @@
+package com.poetic.FluentCantonese.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.poetic.FluentCantonese.R;
+
+public class FlashcardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flashcard);
+    }
+}
