12b60ad76838d41c091e00fe763eb460cabdc425
Daniele Della Cioppa <daniele.dellacioppa@gmail.com>
2023-06-10 18:15:45 +0100

    text is not mirrored anymore but animation is horrible

--- app/src/main/java/com/poetic/FluentCantonese/activities/FlashcardActivity.java
+++ app/src/main/java/com/poetic/FluentCantonese/activities/FlashcardActivity.java
@@ -5,13 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.animation.Animator;
 import android.animation.ObjectAnimator;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.view.animation.AccelerateDecelerateInterpolator;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.poetic.FluentCantonese.R;
 
 public class FlashcardActivity extends AppCompatActivity {
+    private boolean isFront = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,24 +40,42 @@ public class FlashcardActivity extends AppCompatActivity {
     }
 
     private void flipCard() {
-        View flashcardLayout = findViewById(R.id.flashcardLayout);
-
-        // Esegue l'animazione di flip
-        ObjectAnimator flipAnimator = ObjectAnimator.ofFloat(flashcardLayout, "rotationY", 0f, 180f);
-        flipAnimator.setDuration(500);
-        flipAnimator.setInterpolator(new AccelerateDecelerateInterpolator());
-        flipAnimator.start();
-
-        // Inverte la visibilità dei layout fronte e retro
         LinearLayout frontLayout = findViewById(R.id.frontLayout);
         LinearLayout backLayout = findViewById(R.id.backLayout);
-        if (frontLayout.getVisibility() == View.VISIBLE) {
-            frontLayout.setVisibility(View.GONE);
-            backLayout.setVisibility(View.VISIBLE);
+
+        View frontTextView = findViewById(R.id.textViewFront);
+        View backTextView = findViewById(R.id.textViewBack);
+
+        // Effettua l'animazione di flip sulle viste dei layout frontali e posteriori
+        frontLayout.animate().rotationY(90f).setDuration(500);
+        backLayout.animate().rotationY(90f).setDuration(500);
+
+        // Imposta il testo corretto sulla vista frontale
+        if (isFront) {
+            ((TextView) frontTextView).setText("Hello");
         } else {
-            frontLayout.setVisibility(View.VISIBLE);
-            backLayout.setVisibility(View.GONE);
+            ((TextView) frontTextView).setText("你好");
         }
+
+        // Imposta il testo corretto sulla vista posteriore
+        if (isFront) {
+            ((TextView) backTextView).setText("你好");
+        } else {
+            ((TextView) backTextView).setText("Hello");
+        }
+
+        // Effettua un'animazione di flip inversa dopo un ritardo di 250 ms per mostrare il nuovo testo
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                frontLayout.animate().rotationY(0f).setDuration(500);
+                backLayout.animate().rotationY(0f).setDuration(500);
+            }
+        }, 250);
+
+        // Inverte lo stato della flashcard
+        isFront = !isFront;
     }
+
 
 }
